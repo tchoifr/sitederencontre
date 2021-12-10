5778ffa75f05f2f798aab4224bfb0dc7863db569
François Giorgi <francois.giorgi.pro@gmail.com>
2021-12-10 18:23:43 +0100

    3d

--- profil/profil.js
+++ profil/profil.js
@@ -1,15 +1,27 @@
 //Profil
-//
+//renvoi text sur autre id
 let textProfilNom = document.getElementById('textProfilNom');
 let receptionNom = document.getElementById('receptionNom');
+let textProfilInteret = document.getElementById('textProfilInteret');
+let textProfilRecherche = document.getElementById('textProfilRecherche');
+
+
+
 
 textProfilNom.addEventListener('input',()=>{
     receptionNom.innerHTML= textProfilNom.value
     console.log(textProfilNom.value)
 })
 
+textProfilInteret.addEventListener('input',()=>{
+    receptionInteret.innerHTML= textProfilInteret.value
+    console.log(textProfilInteret.value)
+})
 
-
+textProfilRecherche.addEventListener('input',()=>{
+    receptionRecherche.innerHTML= textProfilRecherche.value
+    console.log(textProfilRecherche.value)
+})
 
 //Carousel
 
