f552a43088bee260d1558aefb0ee16e8ad0cc9b5
François Giorgi <francois.giorgi.pro@gmail.com>
2021-12-16 10:45:35 +0100

    profiltest

--- profilHot/profil.js
+++ profilHot/profil.js
@@ -1,27 +1,27 @@
 //Profil
 //renvoi text sur autre id
-let textProfilNom = document.getElementById('textProfilNom');
-let receptionNom = document.getElementById('receptionNom');
-let textProfilInteret = document.getElementById('textProfilInteret');
-let textProfilRecherche = document.getElementById('textProfilRecherche');
+// let textProfilNom = document.getElementById('textProfilNom');
+// let receptionNom = document.getElementById('receptionNom');
+// let textProfilInteret = document.getElementById('textProfilInteret');
+// let textProfilRecherche = document.getElementById('textProfilRecherche');
 
 
 
 
-textProfilNom.addEventListener('input',()=>{
-    receptionNom.innerHTML= textProfilNom.value
-    console.log(textProfilNom.value)
-})
+// textProfilNom.addEventListener('input',()=>{
+//     receptionNom.innerHTML= textProfilNom.value
+//     console.log(textProfilNom.value)
+// })
 
-textProfilInteret.addEventListener('input',()=>{
-    receptionInteret.innerHTML= textProfilInteret.value
-    console.log(textProfilInteret.value)
-})
+// textProfilInteret.addEventListener('input',()=>{
+//     receptionInteret.innerHTML= textProfilInteret.value
+//     console.log(textProfilInteret.value)
+// })
 
-textProfilRecherche.addEventListener('input',()=>{
-    receptionRecherche.innerHTML= textProfilRecherche.value
-    console.log(textProfilRecherche.value)
-})
+// textProfilRecherche.addEventListener('input',()=>{
+//     receptionRecherche.innerHTML= textProfilRecherche.value
+//     console.log(textProfilRecherche.value)
+// })
 
 //Carousel
 
@@ -38,21 +38,28 @@ textProfilRecherche.addEventListener('input',()=>{
 //     }
     
 //     spinner.setAttribute("style","transform:rotateY("+ angle +"deg);")
-// }	
+// }
 
-$('section.awSlider .carousel').carousel({
-    pause: "hover",
-    interval: 2000
+
+
+// $('section.awSlider .carousel').carousel({
+//     pause: "hover",
+//     interval: 2000
+//   });
+  
+//   var startImage = $('section.awSlider .item.active > img').attr('src');
+//   $('section.awSlider').append('<img src="' + startImage + '">');
+  
+//   $('section.awSlider .carousel').on('slid.bs.carousel', function () {
+//    var bscn = $(this).find('.item.active > img').attr('src');
+//     $('section.awSlider > img').attr('src',bscn);
+//   });
+  
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
   });
-  
-  var startImage = $('section.awSlider .item.active > img').attr('src');
-  $('section.awSlider').append('<img src="' + startImage + '">');
-  
-  $('section.awSlider .carousel').on('slid.bs.carousel', function () {
-   var bscn = $(this).find('.item.active > img').attr('src');
-    $('section.awSlider > img').attr('src',bscn);
-  });
-  
   
     
     
